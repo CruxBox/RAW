e557ff1d7cf58a1a45e97f3e0d8d0264e07fb8cb
CruxBox <shubhambhagat111@yahoo.com>
2019-10-09 22:06:20 +0530

    saved images from 4 different channels. completed.

--- try.cpp
+++ try.cpp
@@ -12,48 +12,99 @@ enum colors
     BLUE
 };
 
+int row = 3072, col = 4096;
+
 int main()
 {
-    vector<uint16_t> channels[4];
+	// 16 bit version channels
+    // vector<uint16_t> channels[4];
+	uint16_t r, g, g2, b;
+	
     ifstream data("portrait-gainx2-offset2047-20ms-02.raw12", ios::binary);
     unsigned char buffer[3];
-    uint16_t r, g, g2, b;
 
-    vector<unsigned char> red,green1,green2,blue;
+    //8 bit version channels
+    vector<unsigned char> channels[4];
+    // unsigned char red,green1,green2,blue;
 
     memset(buffer, '\0', sizeof(buffer));
 
-    int i = 0;
+    int iter = 0;
+    int columns_read = 0;
     while (data.read((char *)buffer, sizeof(buffer)))
     {
-        if (i < 2048)
+    	if(columns_read==col) iter++,columns_read=0;
+        if(iter>=row) break;
+    	columns_read+=2;
+
+    	if(iter&1)
         {
-            r = (uint8_t(buffer[0]) | ((uint8_t(buffer[1]) >> 4) & 0x0F));
-            channels[RED].push_back(r);
-            g = ((uint8_t(buffer[1] & 0x0F) << 4) | uint8_t(buffer[2]));
-            channels[GREEN1].push_back(g);
-            i += 2;
+
+        	g2 = (uint16_t(buffer[0])<<4 | ((uint16_t(buffer[1]) >> 4) >> 4));
+            channels[GREEN2].push_back(char(g2/16));
+            b = ((uint16_t(buffer[1] & 0x0F) << 8) | uint8_t(buffer[2]));
+            channels[BLUE].push_back(char(b/16));
+//
+            // green2 = (uint8_t(buffer[0]));
+            // channels[GREEN2].push_back(green2);
+            // green2 = ((uint8_t(buffer[1]) >> 4) & 0x0F);
+            // channels[GREEN2].push_back(green2);
+
+            // blue = ((uint8_t(buffer[1]  & 0x0F ) << 4));
+            // channels[BLUE].push_back(blue);
+            // blue =  uint8_t(buffer[2]);
+            // channels[BLUE].push_back(blue);
         }
-        else if (i >= 2048)
-        {
-            g2 = (uint8_t(buffer[0]) | ((uint8_t(buffer[1]) >> 4) & 0x0F));
-            channels[GREEN2].push_back(r);
-            b = ((uint8_t(buffer[1]  & 0x0F ) << 4) | uint8_t(buffer[2]));
-            channels[BLUE].push_back(g);
-            i += 2;
-            if (i == 4096)
-                i = 0;
+        else{
+
+        	r = (uint16_t(buffer[0])<<4 | ((uint16_t(buffer[1]) >> 4) >> 4));
+            channels[RED].push_back(char(r/16));
+            g = ((uint16_t(buffer[1] & 0x0F) << 8) | uint8_t(buffer[2]));
+            channels[GREEN1].push_back(char(g/16));
+
+            // red = (uint8_t(buffer[0]));
+            // channels[RED].push_back(red);
+            // red =  (uint8_t(buffer[1]) >> 4) & 0x0F;
+            // channels[RED].push_back(red);
+
+            // green1 = (uint8_t(buffer[1] & 0x0F) << 4);
+            // channels[GREEN1].push_back(green1);
+            // green1 =  uint8_t(buffer[2]);
+            // channels[GREEN1].push_back(green1);
         }
-    }
-    for (int i = 0; i < 85; i++)
-    {
-        channels[RED].pop_back();
-        channels[GREEN1].pop_back();
     }
 
-//     cout << channels[RED].size() << endl
-//          << channels[GREEN1].size() << endl
-//          << channels[GREEN2].size() << endl
-//          << channels[BLUE].size() << endl;
+    cout << channels[RED].size() << endl
+         << channels[GREEN1].size() << endl
+         << channels[GREEN2].size() << endl
+         << channels[BLUE].size() << endl;
+
+    ofstream redimage,green1image,green2image,blueimage;
+    redimage.open("redImage.ppm", ios::binary);
+    green1image.open("green1Image.ppm",ios::binary);
+    green2image.open("green2Image.ppm",ios::binary);
+    blueimage.open("blueImage.ppm",ios::binary);
+
+   	redimage << "P6\n2048 1536\n255\n";
+    green1image << "P6\n2048 1536\n255\n";
+    green2image << "P6\n2048 1536\n255\n";
+	blueimage << "P6\n2048 1536\n255\n";
+
+	//all channels have same size
+	int size = channels[RED].size();
+   	for(int i=0;i<size;i++){
+   		redimage << channels[RED][i]<<0<<0;
+   		green1image << 0<<channels[GREEN1][i]<<0;
+   		green2image << 0<<channels[GREEN2][i]<<0;
+   		blueimage << 0<<0<<channels[BLUE][i];
+   	}
+	
+	//close files
+	redimage.close();
+	green1image.close();
+	green2image.close();
+	blueimage.close();
+   	data.close();
+
 
 }
